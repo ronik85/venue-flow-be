e30b32f17e76ca95b1c7edecb0686489e157947d
Ronik <ronik.kalanouria@gmail.com>
2026-05-07 17:02:46 +0530

    postgres setup

---
+++ src/database/database.module.ts
@@ -0,0 +1,31 @@
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT') || '5432', 10),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: false,
+        logging: true,
+      }),
+      dataSourceFactory: async (options) => {
+        const dataSource = new DataSource(options!);
+        await dataSource.initialize();
+        Logger.log('Database connected successfully', 'Database');
+        return dataSource;
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
